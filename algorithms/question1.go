5db4c34c693c559a4c70a4bd4ffeb4ee82d12282
Mirza Karabulut <mirzakara1234@gmail.com>
2024-12-29 16:02:12 +0300

    added if conditions for handle edge cases easily

--- algorithms/question1.go
+++ algorithms/question1.go
@@ -5,14 +5,22 @@ import (
 	"strings"
 )
 
-func Question1(charArr []string) []string {
-	sort.SliceStable(charArr, func(i, j int) bool {
-		countA := strings.Count(charArr[i], "a")
-		countB := strings.Count(charArr[j], "a")
+func Question1(strArr []string) []string {
+	if strArr == nil {
+		return nil
+	}
+
+	if len(strArr) == 0 || len(strArr) == 1 {
+		return strArr
+	}
+
+	sort.SliceStable(strArr, func(i, j int) bool {
+		countA := strings.Count(strArr[i], "a")
+		countB := strings.Count(strArr[j], "a")
 		if countA != countB {
 			return countA > countB
 		}
-		return len(charArr[i]) > len(charArr[j])
+		return len(strArr[i]) > len(strArr[j])
 	})
-	return charArr
+	return strArr
 }
